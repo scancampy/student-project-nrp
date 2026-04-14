f203434febc942a4afb63555bb350a6bea062c2b
Andre <andre@staff.ubaya.ac.id>
2026-04-14 10:48:13 +0700

    Update DetailViewModel to use AndroidViewModel and prepare for Volley integration.
    - Refactor `DetailViewModel` to extend `AndroidViewModel` to access application context.
    - Initialize `RequestQueue` and a tag for managing Volley network requests.
    - Update `fetch()` method to include a remote JSON URL and comment out hardcoded student data.

--- app/src/main/java/com/nmpmiftah/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/nmpmiftah/studentproject/viewmodel/DetailViewModel.kt
@@ -1,16 +1,25 @@
 package com.nmpmiftah.studentproject.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.RequestQueue
 import com.nmpmiftah.studentproject.model.Student
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(application: Application):
+                                        AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    private var TAG = "volley_tag"
+    private var queue: RequestQueue?= null
+
 
     fun fetch() {
-        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
-            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
-        studentLD.value = student1
+        val url =  "https://www.jsonkeeper.com/b/LLMW"
+
+       /* val student1 = Student("16055","Nonie","1998/03/28","5718444778",
+            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")*/
+      //  studentLD.value = student1
     }
 
 }
